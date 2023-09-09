0e3a466a09c3a54e8eeb5ecdc12c3cd8cfd07432
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-09-09 19:57:03 +0200

    🎨 Add main view structure

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,5 +1,22 @@
+import { Days, Header, Highlights, Hours } from '../../components';
 import './Main.scss';
 
 export const Main = () => {
-	return <div>Main</div>;
+	return (
+		<>
+			<main>
+				<Header />
+				<section className="main-section">
+					<h1 className="hidden">Weather App</h1>
+					<h2>Today's highlights</h2>
+					<Highlights />
+
+					<h2>Hourly Forecast</h2>
+					<Hours />
+
+					<Days />
+				</section>
+			</main>
+		</>
+	);
 };
