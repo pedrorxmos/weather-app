088ede3f87f9782116358a239140a8c96870972c
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-09-09 23:49:39 +0200

    💄 Add icon component html and inner svg content

--- src/components/sidebar/Sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar/Sidebar.tsx
@@ -1,5 +1,10 @@
+import { Icon } from '../..';
 import './Sidebar.scss';
 
 export const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div>
+			Sidebar <Icon name="water" size="sm" />
+		</div>
+	);
 };
